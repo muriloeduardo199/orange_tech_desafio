e63fd2827ca04996e5f99bce0e78a38ab7c6094a
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-11-16 19:12:49 -0300

    updatr

---
+++ funcoes/aula_funcao.js
@@ -0,0 +1,25 @@
+function saymyname(name) {
+    console.log('meu nome e ' + name);
+}
+
+function quadrado(valor){
+   return valor*valor;
+    
+}
+
+
+function incrementarvalor(valor, percentualjuros) {
+     const valordeacrescimo = (percentualjuros/ 100) *valor;
+     return valor+valordeacrescimo;
+
+}
+
+
+
+
+function incrementarJuros(valor, percentualDeJuros) {
+    const incremento = (percentualDeJuros / 100) * valor;
+    return valor + incremento;
+}
+
+inc
---
+++ variaveis_operadores/aula.js
@@ -0,0 +1,10 @@
+let variavel = 10/10;
+console.log(variavel);
+
+const precoCombustivel = 5.79;
+const kmPorLitros = 12;
+const distanciaEmKm = 100;
+
+const litrosConsumidos = distanciaEmKm / kmPorLitros;
+const valorGasto = litrosConsumidos * precoCombustivel;
+console.log(valorGasto.toFixed(2));
