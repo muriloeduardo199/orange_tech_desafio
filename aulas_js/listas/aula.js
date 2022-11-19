eeb84298393abd316f02be8b8ab9c099886da2da
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-11-19 18:48:19 -0300

    update

---
+++ aulas_js/listas/aula.js
@@ -0,0 +1,19 @@
+const notas = [];
+
+notas.push(5);
+notas.push(8);
+notas.push(7);
+notas.push(6);
+notas.push(4);
+notas.push(6);
+notas.push(10);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma += nota;
+}
+
+const media = soma / notas.length;
+console.log(media);
---
+++ aulas_js/listas/exercicio.js
@@ -0,0 +1,44 @@
+
+// const num = 5
+// for (let i = 0; i < 10; i++) {
+//     console.log(i*num);
+    
+// }
+
+// const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+// for (let i= 0; i< numeros.length; i++) {
+//     const element = numeros[i];
+//     console.log(element)
+// }
+
+// const numerosPares = [];
+
+//     for (let i = 0; i < 10; i++) {
+//         const numeroPar = i % 2 === 0;
+//         if (numeroPar) {
+//             numerosPares.push(i);
+//         }
+//     }
+
+// console.log(numerosPares)
+
+// const listaDeValores = [1, 2, 3, 4, 5];
+//     const valor = listaDeValores[2];
+//     console.log(valor);
+
+// const numeros = [];
+
+// for (let i = 0; i < 10; i++) {
+//     numeros.push(i);
+// }
+
+// console.log(numeros);
+
+const numerosPares = [];
+
+for (let i = 0; i < 10; i = i + 2) {
+    numerosPares.push(i);
+}
+
+console.log(numerosPares);
