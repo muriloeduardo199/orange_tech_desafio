8b6c8326c497990884ceb2b35d190e3e4d36ff74
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-11-23 18:13:12 -0300

    update

---
+++ react/dio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/button';
+
+
+function App() {
+  return (
+  <div className='App'>
+    <h1>ola react</h1>
+    <Button title="entrar"/>
+    <Button title="sair"/>
+    <Button title="editar"/>
+  </div>
+ 
+
+  );
+}
+
+export default App;
